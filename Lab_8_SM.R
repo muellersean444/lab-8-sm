74e187951ee4c5646397c908918211459b3bf971
Sean Mueller <seanmueller@169-231-173-181.wireless.ucsb.edu>
2018-03-08 14:55:27 -0800

    commiting again (with leadflet code)

--- Lab_8_SM.R
+++ Lab_8_SM.R
@@ -22,3 +22,16 @@ colnames(df) <- c("Year", "Loss")
 
 ggplot(df, aes(x = Year, y = Loss)) +
   geom_col()
+
+###Leaflet plot of spill locations in TX in 2016
+df_loc <- oil_spills %>% 
+  filter(`Accident State` == "TX" & `Accident Year` == 2016) %>% 
+  select(Latitude, Longitude, `Net Loss (Barrels)`)
+
+colnames(df_loc) <- c("latitude", "longitude", "net_loss")
+
+oil_sf <- st_as_sf(df_loc, coords = c("longitude", "latitude"), crs = 4326)
+
+leaflet(oil_sf) %>% 
+  addTiles() %>% 
+  addMarkers()
